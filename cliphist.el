3105e5c4b4d2d0338edb6effd9329426854b80b1
Chen Bin <chenbin.sh@gmail.com>
2019-09-20 11:49:51 +1000

    flycut doc

--- cliphist.el
+++ cliphist.el
@@ -33,6 +33,8 @@
 ;;
 ;; Usage:
 ;;   Make sure clipboard manager is running.
+;;   If you use Flycut on macOS, set up "Preferences > General > Clippings",
+;;   so its value is "Save After each clip".
 ;;   `M-x cliphist-paste-item' to paste item from history
 ;;   `C-u M-x cliphist-paste-item' rectangle paste item
 ;;   `M-x cliphist-select-item' to select item
